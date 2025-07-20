40dbe1b1bfdce1d61cd75ee641d7eea377a64087
M-GreySun <mgreysun@gmail.com>
2025-07-20 15:19:52 -0400

    Added the log to get the human choice. I had to search/ figure out how to use prompt

--- logic.js
+++ logic.js
@@ -1,9 +1,9 @@
 console.log("Hello, from the javascript file");
-
+const prompt = require("prompt-sync")();
 function getComputerChoice(){
     let choice= Math.random();      //Will return a value greater than or equal to 0 and less than 1
 
-    console.log(`The value of choice ${choice}`);
+    //console.log(`The value of choice ${choice}`);
 
     if(choice <(1/3)){
         return "Rock";
@@ -14,6 +14,12 @@ function getComputerChoice(){
     return "Scissors";
 }
 
-for (let i=0; i< 10; i++){
-    console.log(getComputerChoice());
+function getHumanChoice(choice ="Invalid Answer"){
+    choice =prompt("Please enter a valid choice (1 for Rock, 2 for Paper, or 3 for Scissors): ");
+
+    console.log(choice);
+
+    return choice;
 }
+
+getHumanChoice();
