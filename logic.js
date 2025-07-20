76b66708530e4340a9e4f206f1fefbddef0874fb
M-GreySun <mgreysun@gmail.com>
2025-07-20 14:49:25 -0400

    Added the logic to get the coputer choice

--- logic.js
+++ logic.js
@@ -1,1 +1,19 @@
 console.log("Hello, from the javascript file");
+
+function getComputerChoice(){
+    let choice= Math.random();      //Will return a value greater than or equal to 0 and less than 1
+
+    console.log(`The value of choice ${choice}`);
+
+    if(choice <(1/3)){
+        return "Rock";
+    }
+    else if(choice>= (1/3) && choice< (2/3)){
+        return "Paper";
+    }
+    return "Scissors";
+}
+
+for (let i=0; i< 10; i++){
+    console.log(getComputerChoice());
+}
