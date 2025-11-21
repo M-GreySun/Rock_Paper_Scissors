faa3161cb0a73a9ad833d5bddb4f5068b0dc8870
M-GreySun <mgreysun@gmail.com>
2025-11-21 02:41:21 -0500

    The results are currently displaying. However I do not like how it looks as of now. I believe everything else is working okay. The display transitioning between results, reset, and options buttons are currently not the best. I'll work on formatting the results later. I also want to point out that that results are pretty much all handled in the js file to get more practice working with dom elements.

--- logic.js
+++ logic.js
@@ -2,7 +2,7 @@ let player=0;
 let AI= 0;
 let tie=0;
 
-//let numberOfRounds=5;
+
 
 function geAIChoice(){
     let choice= Math.random();      //Will return a value greater than or equal to 0 and less than 1
@@ -17,9 +17,6 @@ function geAIChoice(){
 }
 
 function getPlayerChoice(){
-    //const prompt = require("prompt-sync")();
-    //let choice =prompt("Please enter a valid choice (Rock, Paper, Scissors): ");
-
     if(choice.toLowerCase("rock")=== "rock"){
         return "rock"
     }
@@ -29,101 +26,93 @@ function getPlayerChoice(){
     else{
         return "scissors";
     }
-    //return "Invalid Choice";
 }
 
 function playRound(playerChoice, AIChoice){
-    let results= document.querySelector(".round");
+    
+    let results= document.querySelector(".results");
+    
+    
+    results.setAttribute("style", "visibility: visible;");
+    
+    
     if(playerChoice===AIChoice){
         tie++;
         results.textContent= `I'ts a draw both players chose ${playerChoice}!`;
-        //document.querySelector(".score #tie").textContent= `${tie}`;
 
     }
-    //else if(playerChoice=== "Invalid Choice"){
-        //AI++;
-        //results.textContent= `Player entered an invalid choice: ${playerChoice}. You Lose!`;
-        //document.querySelector(".score #computer").textContent= `Computer Score: ${AI}`;
-
-    //}
+    
     else if((playerChoice=== "rock" && AIChoice== "scissors") || 
             (playerChoice=== "paper" && AIChoice== "rock") || 
             (playerChoice=== "scissors" && AIChoice== "paper")){
         player ++;
         results.textContent= `You win ${playerChoice} beats ${AIChoice}!`;
         document.querySelector(".scoreValue#player").textContent= `${player}`;
-        //class="scoreValue" id="player"
     }
     else{
         AI++;
         results.textContent= `You lose ${AIChoice} beats ${playerChoice}!`;
         document.querySelector(".scoreValue#AI").textContent= `${AI}`;
-        //class="scoreValue" id="AI"
     }
 }
 
-/*
-function playGame(){
-    for(let i=0; i< numberOfRounds; i++){
-        playRound(getPlayerChoice(), geAIChoice());
-    }
-    
-    console.log("Final Results: \n");
 
-    console.log(`After ${numberOfRounds} round.`);
-    console.log("Human Scored: " + player);
-    console.log("Computer Score: "+ AI);
-    console.log(`Draws/ Ties: ${tie}`);
+
+function resetGame(){
+    player=0;
+    AI= 0;
+    tie=0;
+    document.querySelector(".scoreValue#player").textContent= `${player}`;
+    document.querySelector(".scoreValue#AI").textContent= `${player}`;
+
 }
-*/
-function finalResults(){
-    let final= document.querySelector(".final");
+function displayResults(){
+    let display= document.querySelector(".results");
+    display.textContent="";
     let paragraph= document.createElement("p");
 
-    paragraph.textContent= `After ${numberOfRounds} round the final results are as follows: `;
-    final.appendChild(paragraph);
+    paragraph.textContent= `After ${player+ AI+ tie} round the final results are as follows: `;
+    display.appendChild(paragraph);
 
     paragraph= document.createElement("p");
     paragraph.textContent= `Human Scored: ${player}`;
-    final.appendChild(paragraph);
+    display.appendChild(paragraph);
 
     paragraph= document.createElement("p");
     paragraph.textContent= `Computer Score: ${AI}`;
-    final.appendChild(paragraph);
+    display.appendChild(paragraph);
 
     paragraph= document.createElement("p");
     paragraph.textContent= `Draws/ Ties: ${tie}`;
-    final.appendChild(paragraph);
+    display.appendChild(paragraph);
 
+    paragraph=document.createElement("p");
+
+    if(player>AI){
+        paragraph.textContent("\nPlayer Wins!");
+    }
+    else if(player<AI){
+        paragraph.textContent("AI Wins!")
+    }
+    else{
+        paragraph.textContent("It's a Draw!");
+    }
+    display.append(paragraph);
 }
+
 
 let button= document.querySelectorAll("button");
 button.forEach((ele)=>{
     ele.addEventListener("click", () => {
-        //playRound(ele.id, geAIChoice());
-        //if((player+ AI+ tie)==numberOfRounds){
-            //finalResults();
-        //}
         if(ele.id !="result" && ele.id != "reset"){
             playRound(ele.id, geAIChoice());
         }
         else if(ele.id =="reset"){
-            player=0;
-            AI= 0;
-            tie=0;
-            document.querySelector(".scoreValue#player").textContent= `${player}`;
-            document.querySelector(".scoreValue#AI").textContent= `${player}`;
+            resetGame();
+            document.querySelector(".results").setAttribute("style", "visibility: hidden");
         }
-        console.log(`The id of the button that was clicked: ${ele.id}`);
+        else{
+            displayResults();
+        }
     });        
 });
-
-/*
-let score= document.querySelectorAll(".score div")
-score.forEach((ele)=> {
-    console.log(ele.id);
-});
-*/
-
-
-//playGame();
